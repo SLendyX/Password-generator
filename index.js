1c30a61b92fbd03f5c74d7dba32afc86b738674d
SLendyX <slendyx2002@gmail.com>
2024-01-19 14:45:45 +0200

    added randomgenerator and copy on click

--- index.js
+++ index.js
@@ -9,8 +9,52 @@ const characters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","
 
 
 let firstPasswordEl = document.getElementById("password1-el");
-firstPasswordEl.textContent="Hello"
+let secondPasswordEl = document.getElementById("password2-el");
+
+let generateEl = document.getElementById("getpassword")
+
+function getRandom(){
+    return Math.floor(Math.random()*characters.length);
+}
+
+generateEl.addEventListener("click", function() {
+
+    firstPasswordEl.textContent = "";
+    secondPasswordEl.textContent = "";
+
+    for(let i=0; i<18; i++){
+        let randomIndex = getRandom();
+        firstPasswordEl.textContent += characters[randomIndex];
+        randomIndex = getRandom();
+        secondPasswordEl.textContent += characters[randomIndex];
+    }
+
+})
 
 
+firstPasswordEl.addEventListener("click", async function(){ 
+    navigator.clipboard.writeText(firstPasswordEl.textContent)
 
+    firstPasswordEl.style.animationName="copy";
+
+    await delay(1000)
+
+    firstPasswordEl.style.animationName="none";
+})
+
+
+secondPasswordEl.addEventListener("click", async function(){ 
+    navigator.clipboard.writeText(secondPasswordEl.textContent)
+
+    secondPasswordEl.style.animationName="copy";
+
+    await delay(1000)
+
+    secondPasswordEl.style.animationName="none";
+})
+
+
+function delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+  }
 
